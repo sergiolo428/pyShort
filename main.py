bc56f84da6b85fecb593b08c5c68a12a37797666
sergiolo428 <sergiolo428@gmail.com>
2022-11-16 11:36:06 +0100

    Update

---
+++ main.py
@@ -0,0 +1,23 @@
+# PyShort
+import numpy as np
+import os
+import sys
+os.chdir(sys.path[0]) # Change to current .py file path
+
+def see(arr):
+    for i in arr:
+        print(i)
+
+def short1(arr):
+    piv=arr[len(arr)-1]
+    end=len(arr)
+    for i in range(0,end):
+        if arr[i]>piv:
+            print(arr[i])
+            arr=np.delete(arr,arr[i])
+            i=i-1
+            arr=np.append(arr,arr[i])
+    see(arr)
+
+with open("values.txt","r") as f:
+    f.read
