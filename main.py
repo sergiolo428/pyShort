3492e310b42376e3f06c9f74b3f8b3a07755960c
sergiolo428 <sergiolo428@gmail.com>
2022-11-16 19:08:08 +0100

    Added File reader + test file(values.txt)

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import os
 import sys
 os.chdir(sys.path[0]) # Change to current .py file path
 
+# See data
 def see(arr):
     for i in arr:
         print(i)
 
+# Shorting (Not working rn)
 def short1(arr):
     piv=arr[len(arr)-1]
     end=len(arr)
@@ -19,7 +21,20 @@ def short1(arr):
             arr=np.append(arr,arr[i])
     see(arr)
 
-with open("values.txt","r") as f:
-    f.read
+# Get values from files
+def extract():
+    data=[]
+    with open("values.txt","r") as f:
+        for line in f:
+            data.append(line.split())
+    return data
 
-    
+def flatten(info):
+    final_list=[]
+    for  i in info:
+        for j in i:
+            final_list.append(j)
+    return final_list
+
+info=flatten(extract())
+see(info)
